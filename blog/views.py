f8c60135b34477ba2279d81baaa34dc44ce48f79
andrecas2 <nextconcept0@gmail.com>
2018-11-04 15:13:30 -0600

    Publicaciones Como Borrador.

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,7 @@ def post_new(request):
             if form.is_valid():
                 post = form.save(commit=False)
                 post.autor = request.user
-                post.fecha_publicacion = timezone.now()
+                #post.fecha_publicacion = timezone.now()
                 post.save()
                 return redirect('detalle', pk=post.pk)
         else:
@@ -38,3 +38,17 @@ def post_edit(request, pk):
         else:
             form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_publicacion')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('detalle', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_draft_list')
